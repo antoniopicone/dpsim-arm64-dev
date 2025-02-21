2696c8b1ab364bf139bb8fd57fcec84ef3af8b8d
gisaba <gisaba1980@gmail.com>
2025-02-21 14:19:35 +0100

    freq

--- Villas/test_with_docker_compose/dpsim_lab_a_NEW.py
+++ Villas/test_with_docker_compose/dpsim_lab_a_NEW.py
@@ -46,12 +46,12 @@ def start_simulation(voltage_phasor,current_step_prec,sequence):
     #print("current_step_prec : ", current_step_prec)
 
     #Vn1 = ((current_step_prec * complex(r1.R,l1.L*FREQUENZA*2*math.pi)) + voltage_phasor)
-    Vn1 = ((current_step_prec * complex(r1.R,l1.L)) + voltage_phasor)
+    Vn1 = ((current_step_prec * complex(r1.R,l1.L*2*FREQUENZA)) + voltage_phasor)
     #print("Applying voltage node n1 : ", Vn1)
     n1.set_initial_voltage(Vn1)
     
     #Vn2 = (current_step_prec * complex(0,l1.L*FREQUENZA*2*math.pi) + voltage_phasor)
-    Vn2 = (current_step_prec * complex(0,l1.L) + voltage_phasor)
+    Vn2 = (current_step_prec * complex(0,l1.L*2*FREQUENZA) + voltage_phasor)
     #print("Applying voltage node n2 : ", Vn2)
     n2.set_initial_voltage(Vn2)
     
